49dd46ca68956cd3268267b5c97ff85ac42ab8d7
Jonas <j.peterburs@outlook.de>
2020-05-25 10:04:52 +0200

    Use new console_helper for start_menu

--- game_of_life.c
+++ game_of_life.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "console_helper.h"
 #include "game_of_life.h"
 
 struct options start_menu()
 {
     struct options current_options;
 
-    printf("\t\tWelcome to the game of life\n\n");
+    allow_special_chars();
+    build_frame(80, 15);
 
-    printf("\tPlease input your game settings.\n");
+    set_cursor(25, 2);
+    printf("Welcome to the game of life");
+
+    set_cursor(10, 5);
+    printf("Please input your game settings.");
+
+    set_cursor(5, 6);
     printf("Height: ");
     scanf("%i", &current_options.height);
     fflush(stdin);
 
+    set_cursor(5, 7);
     printf("Width: ");
     scanf("%i", &current_options.width);
     fflush(stdin);
 
+    set_cursor(5, 8);
     printf("Iterations per second: ");
     scanf("%i", &current_options.iterations_per_second);
     fflush(stdin);
 
-    printf("\n");
 
-    printf("\tPlease select your characters.\n");
+    set_cursor(10, 10);
+    printf("\tPlease select your characters.");
+
+    set_cursor(5, 11);
     printf("Alive cells: ");
     scanf("%c", &current_options.alive);
     fflush(stdin);
 
+    set_cursor(5, 12);
     printf("Dead cells: ");
     scanf("%c", &current_options.dead);
     fflush(stdin);
--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "console_helper.h"
 #include "game_of_life.h"
 
 int main()
 {
     struct options current_options = start_menu();
 
+    set_cursor(5, 20);
     printf("[%ix%i] %iIPS: %c %c", current_options.height, current_options.width, current_options.iterations_per_second, current_options.alive, current_options.dead);
+
     return 0;
 }
